94802b1cc86a9d4668fbbe6215d7ab2f118b824d
juyeong-chungbuk <happyu9986@gmail.com>
2022-06-28 18:23:29 +0900

    initial setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/reset.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <section></section>
+    </div>
+  );
+}
+
+export default App;

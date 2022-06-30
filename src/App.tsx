9331f0b23acf3f9abb6d5d46be0bd3421454077c
harloxx <chloe0105@naver.com>
2022-06-30 16:16:59 +0900

    feat:login skel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Login from './Login';
 import './styles/reset.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <section></section>
+      <header className="App-header">login</header>
+      <section>
+        <Login />
+      </section>
     </div>
   );
 }
---
+++ src/Login.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+  const [inputId, setInputId] = useState('');
+  const [inputPw, setInputPw] = useState('');
+
+  const handleInputId = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputId(e.target.value);
+    console.log(inputId);
+  };
+
+  const handleInputPw = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputPw(e.target.value);
+  };
+
+  const onClickSubmit = (e: any): void => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        name="id"
+        value={inputId}
+        placeholder="아이디를 입력해주세요"
+        onChange={handleInputId}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="비밀번호를 입력해주세요"
+        value={inputPw}
+        onChange={handleInputPw}
+      />
+      <button type="submit" onClick={onClickSubmit}>
+        로그인
+      </button>
+    </div>
+  );
+}
+
+export default Login;

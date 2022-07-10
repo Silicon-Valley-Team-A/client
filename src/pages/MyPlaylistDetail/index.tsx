c9be8f1927763d5ac868691c5a1d3af5312b5c57
harloxx <chloe0105@naver.com>
2022-07-11 07:45:56 +0900

    feat:재생 컴포넌트 생성

--- src/pages/MyPlaylistDetail/index.tsx
+++ src/pages/MyPlaylistDetail/index.tsx
@@ -1,10 +1,40 @@
-import React, { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import HashTag from '../../components/HashTag';
 import MusicPlayer from '../../components/MusicPlayer';
 import Table from '../../components/Table';
 import $ from './style.module.scss';
+import { songsdata } from '../../components/MusicPlayer/audio';
 
 export default function MyPlayListDetail() {
+  const [songs, setSongs] = useState(songsdata);
+  const [isPlaying, setisPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songsdata[0]);
+
+  const audioElem = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioElem.current) {
+      if (isPlaying) {
+        audioElem.current.play();
+      } else {
+        audioElem.current.pause();
+      }
+    }
+  }, [isPlaying]);
+
+  const onPlaying = () => {
+    if (audioElem.current) {
+      const duration = audioElem.current.duration;
+      const ct = audioElem.current.currentTime;
+
+      setCurrentSong({
+        ...currentSong,
+        progress: (ct / duration) * 100,
+        length: duration,
+      });
+    }
+  };
+
   return (
     <div className={$.container_detail}>
       <div className={$.container}>
@@ -22,7 +52,15 @@ export default function MyPlayListDetail() {
           <Table />
         </section>
       </div>
-      <MusicPlayer isPlaying={true} currentSong="Rain" currentSinger="태연" />
+      <audio ref={audioElem} src={currentSong.url} onTimeUpdate={onPlaying} />
+      <MusicPlayer
+        songs={songs}
+        isPlaying={isPlaying}
+        setisplaying={setisPlaying}
+        audioElem={audioElem}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+      />
     </div>
   );
 }
--- src/types/playlist.tsx
+++ src/types/playlist.tsx
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export type PlayList = {
   title: string;
   singer: string;
@@ -7,3 +9,28 @@ export type AllPlayList = {
   id: number;
   playlist: PlayList[];
 };
+
+export type Song = {
+  title: string;
+  singer: string;
+  url: string;
+  progress: number;
+  length: number;
+};
+export type Songs = {
+  title: string;
+  singer: string;
+  url: string;
+  progress: number;
+  length: number;
+}[];
+
+export type setCurrentSong = React.Dispatch<
+  React.SetStateAction<{
+    title: string;
+    singer: string;
+    url: string;
+    progress: number;
+    length: number;
+  }>
+>;

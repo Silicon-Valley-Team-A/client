acce3903f8a9d270f47e17a112c17dd916249a86
harloxx <chloe0105@naver.com>
2022-07-14 13:39:49 +0900

    del:중첩 라우팅 제거

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import MyPlayList from './pages/MyPlaylist';
 import MyPlayListDetail from './pages/MyPlaylistDetail';
 import Signin from './pages/Auth/Signin';
 import Signup from './pages/Auth/Signup';
-import AuthRoute from './route/AuthRoute';
 import { useAppSelector } from './store';
 import MusicPlayer from './components/MusicPlayer';
 import { Header, Footer } from './components/Layout';
@@ -21,10 +20,8 @@ function App() {
           <Route path="/*" element={<MainPage />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/myplaylist" element={<AuthRoute />}>
-            <Route path="/myplaylist" element={<MyPlayList />} />
-            <Route path="/myplaylist/:id" element={<MyPlayListDetail />} />
-          </Route>
+          <Route path="/myplaylist" element={<MyPlayList />} />
+          <Route path="/myplaylist/:id" element={<MyPlayListDetail />} />
         </Routes>
         <Footer />
       </div>
--- src/route/AuthRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { isLogin } from '../utils/isLogin';
-
-const AuthRoute = () => {
-  return isLogin() ? <Outlet /> : <Navigate to="/signin" />;
-};
-
-export default AuthRoute;

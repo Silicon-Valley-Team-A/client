352e5b9dac1eb066880c1f18ff0a7d9fb544197e
harloxx <chloe0105@naver.com>
2022-07-14 12:10:44 +0900

    chore:add signup alert

--- src/pages/Auth/Signup/index.tsx
+++ src/pages/Auth/Signup/index.tsx
@@ -48,6 +48,7 @@ export default function Signup() {
       Register(signupUser)
         .then(res => {
           if (res.result === 'User created successfully') {
+            alert('회원가입이 완료되었습니다');
             navigate('/signin');
           } else if (res.error === 'User already exists') {
             alert('이미 가입된 계정입니다');

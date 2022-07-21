51dac891ab776465996d3d6880a37575c17fa694
harloxx <chloe0105@naver.com>
2022-07-21 17:57:43 +0900

    hotfix:map undefined 예외처리

--- src/pages/MyPlaylist/index.tsx
+++ src/pages/MyPlaylist/index.tsx
@@ -16,6 +16,7 @@ export default function MyPlayList() {
     if (userId) {
       LoadMyPlayList({ user_id: userId })
         .then(response => {
+          console.log(response);
           const responseAllPlaylist: PlayListThumb[] = response.playlist;
           setAllPlaylist(responseAllPlaylist);
           setUserName(response.username);
@@ -36,7 +37,7 @@ export default function MyPlayList() {
         <div className={$['playlist-title']}>{userName}의 Playlists</div>
         <hr />
         <div className={$['playlist-container']}>
-          {allPlaylist.map((playlist: PlayListThumb, index) => {
+          {allPlaylist?.map((playlist: PlayListThumb, index) => {
             return (
               <Link to={'/myplaylist/' + playlist.id} key={playlist.id}>
                 <div className={$['playlist-center']}>
